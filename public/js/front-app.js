75f4b599ef16e3e638a9a84ba7d502b5ff0c81a7
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-06-09 16:26:36 +0200

    aggiunto name al template vue di HomeComponent

--- public/js/front-app.js
+++ public/js/front-app.js
@@ -1944,7 +1944,9 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
-/* harmony default export */ __webpack_exports__["default"] = ({});
+/* harmony default export */ __webpack_exports__["default"] = ({
+  name: 'HomeComponent'
+});
 
 /***/ }),
 
--- resources/js/pages/HomeComponent.vue
+++ resources/js/pages/HomeComponent.vue
@@ -14,7 +14,7 @@
 
 <script>
 export default {
-
+    name:'HomeComponent'
 }
 </script>
 
